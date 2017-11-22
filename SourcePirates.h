fde3367c4a7459ec9a06d7e9ac529abbd450a723
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-11-22 17:59:10 +0100

    AG - add Methods: 	void printMap(); 	std::vector<std::vector<int>> *getMap();

--- SourcePirates.h
+++ SourcePirates.h
@@ -9,46 +9,78 @@
 using json = nlohmann::json;
 
 class SourcePirates {
-public:
-    explicit SourcePirates(std::string strIoFolder);
+	private:
 
-    int **getMap() {};
+		explicit SourcePirates(std::string strIoFolder);
 
-    int getMapFieldByCoords(int numX, int numY) {};
-    int numTurn = 0;
-    std::string arrPlayer;
-    std::string arrPlayers;
-    std::vector<std::vector<int>> *arrMap;
-    std::string arrSpecials;
-    std::string strIoFolder;
-    std::string arrOutput;
-    std::string arrLocalSession;
+	public:
+
+		int getMapFieldByCoords(int numX, int numY);
+		int numTurn = 0;
+
+		std::vector<std::vector<int>> *arrMap;
+		std::vector<std::vector<int>> *getMap();
+
+		std::string arrPlayer;
+		std::string arrPlayers;
+		std::string arrSpecials;
+		std::string strIoFolder;
+		std::string arrOutput;
+		std::string arrLocalSession;
+
+		void printMap();
 
 };
 
 #endif // SOURCEPIRATES_H
 
 
+/**
+ * @brief SourcePirates::SourcePirates Read Json
+ * @param strIoFolder Path to input.JSON
+ */
 SourcePirates::SourcePirates(std::string strIoFolder) {
 
-    std::ifstream jsonFile("../test.json");
-    json input;
-    jsonFile >> input;
+	std::ifstream jsonFile(strIoFolder);
+	json input;
+	jsonFile >> input;
 
-
-    //Create Map Array
-
+	//Create Map Array
 	// 0 = Frei // 1 = Gesperrt
-    arrMap = new std::vector<std::vector<int>>(64, std::vector<int>(64));
-    //Fill vector vector matrix => map
-    int x(0), y(0);
-    for (auto val1 : input["map"]) {
-        for (auto val : val1) {
+	arrMap = new std::vector<std::vector<int>>(64, std::vector<int>(64));
+	//Fill vector vector matrix => map
+	unsigned int x(0), y(0);
+	for (auto val1 : input["map"]) {
+		for (auto val : val1) {
 			arrMap->at(y).at(x) = val;
-            y++;
-        }
-        x++;
-        y = 0;
-    }
+			y++;
+		}
+		x++;
+		y = 0;
+	}
+}
 
+/**
+ * @brief SourcePirates::getMap Return Map Matrix
+ * @return vector<vector<int>>
+ */
+std::vector<std::vector<int>> *SourcePirates::getMap()
+{
+	return arrMap;
+}
+
+/**
+ * @brief SourcePirates::printMap
+ * @brief Print Map into Console
+ */
+void SourcePirates::printMap()
+{
+	for(unsigned int i(0); i<64;i++){
+		std::cout<<i<<"\t";
+		for(unsigned int j(0);j<64;j++){
+			std::cout<<arrMap->at(i).at(j)<<"";
+		}
+		std::cout<<"\n";
+
+	}
 }
--- app.cpp
+++ app.cpp
@@ -5,18 +5,12 @@ using namespace std;
 using json = nlohmann::json;
 
 int main() {
-	SourcePirates gameObj("");
+	SourcePirates gameObj("../test.json");
 
 
 	//cout << gameObj.arrMap->at(32).at(32) << endl;
 
-	for(int i(0); i<64;i++){
-		cout<<i<<"\t";
-		for(int j(0);j<64;j++){
-			cout<<gameObj.arrMap->at(i).at(j)<<"";
-		}
-				   cout<<"\n";
+	gameObj.printMap();
 
-		}
-		return 0;
-	}
+	return 0;
+}
