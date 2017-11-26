010130bfbae14b9b78ca1e1ae00047a164c47099
DESKTOP-NIBRFP7\Michel <michel.k@gmx.ch>
2017-11-26 08:49:27 +0100

    MK - Add coin struct and specials

--- SourcePirates.h
+++ SourcePirates.h
@@ -8,27 +8,42 @@
 
 using json = nlohmann::json;
 
+
+struct Coin {
+    int pos_x;
+    int pos_y;
+
+    Coin(int pos_x, int pos_y) : pos_x(pos_x), pos_y(pos_y) {};
+};
+
+struct Specials {
+    std::vector<Coin> coins;
+};
+
+
 class SourcePirates {
-	private:
+private:
 
-		explicit SourcePirates(std::string strIoFolder);
+public:
 
-	public:
+    explicit SourcePirates(std::string strIoFolder);
 
-		int getMapFieldByCoords(int numX, int numY);
-		int numTurn = 0;
+    //int getMapFieldByCoords(int numX, int numY);
+    Specials specials;
+    int numTurn = 0;
 
-		std::vector<std::vector<int>> *arrMap;
-		std::vector<std::vector<int>> *getMap();
+    std::vector<std::vector<int>> *arrMap;
 
-		std::string arrPlayer;
-		std::string arrPlayers;
-		std::string arrSpecials;
-		std::string strIoFolder;
-		std::string arrOutput;
-		std::string arrLocalSession;
+    std::vector<std::vector<int>> *getMap();
 
-		void printMap();
+    std::string arrPlayer;
+    std::string arrPlayers;
+    std::string arrSpecials;
+    std::string strIoFolder;
+    std::string arrOutput;
+    std::string arrLocalSession;
+
+    void printMap();
 
 };
 
@@ -41,46 +56,49 @@ class SourcePirates {
  */
 SourcePirates::SourcePirates(std::string strIoFolder) {
 
-	std::ifstream jsonFile(strIoFolder);
-	json input;
-	jsonFile >> input;
+    std::ifstream jsonFile(strIoFolder);
+    json input;
+    jsonFile >> input;
 
-	//Create Map Array
-	// 0 = Frei // 1 = Gesperrt
-	arrMap = new std::vector<std::vector<int>>(64, std::vector<int>(64));
-	//Fill vector vector matrix => map
-	unsigned int x(0), y(0);
-	for (auto val1 : input["map"]) {
-		for (auto val : val1) {
-			arrMap->at(y).at(x) = val;
-			y++;
-		}
-		x++;
-		y = 0;
-	}
+    //Create Map Array
+    // 0 = Frei // 1 = Gesperrt
+    arrMap = new std::vector<std::vector<int>>(64, std::vector<int>(64));
+    //Fill vector vector matrix => map
+    unsigned int x(0), y(0);
+    for (auto val1 : input["map"]) {
+        for (auto val : val1) {
+            arrMap->at(y).at(x) = val;
+            y++;
+        }
+        x++;
+        y = 0;
+    }
+
+    // Specials
+    for (auto val : input["specials"]["coin"]) {
+        specials.coins.emplace_back(Coin(val["pos_x"], val["pos_y"]));
+    }
 }
 
 /**
  * @brief SourcePirates::getMap Return Map Matrix
  * @return vector<vector<int>>
  */
-std::vector<std::vector<int>> *SourcePirates::getMap()
-{
-	return arrMap;
+std::vector<std::vector<int>> *SourcePirates::getMap() {
+    return arrMap;
 }
 
 /**
  * @brief SourcePirates::printMap
  * @brief Print Map into Console
  */
-void SourcePirates::printMap()
-{
-	for(unsigned int i(0); i<64;i++){
-		std::cout<<i<<"\t";
-		for(unsigned int j(0);j<64;j++){
-			std::cout<<arrMap->at(i).at(j)<<"";
-		}
-		std::cout<<"\n";
+void SourcePirates::printMap() {
+    for (unsigned int i(0); i < 64; i++) {
+        std::cout << i << "\t";
+        for (unsigned int j(0); j < 64; j++) {
+            std::cout << arrMap->at(i).at(j) << "";
+        }
+        std::cout << "\n";
 
-	}
+    }
 }
